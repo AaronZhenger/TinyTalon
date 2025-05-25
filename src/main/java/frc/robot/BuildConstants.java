66f08966562078efe9341d5f5fad520dc8475056
Aaron Zheng <aaronz.personal@gmail.com>
2025-05-25 00:37:07 -0500

    Initial pc switch commit

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,14 +5,14 @@ package frc.robot;
  */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "Vesper2024";
+  public static final String MAVEN_NAME = "TinyTalon";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 4;
-  public static final String GIT_SHA = "c496b5f8245ab8454f177c59f61088ad04a25571";
-  public static final String GIT_DATE = "2025-05-22 23:45:00 CDT";
+  public static final int GIT_REVISION = 5;
+  public static final String GIT_SHA = "c97d2f288836f634ca8625f25932c008591b7390";
+  public static final String GIT_DATE = "2025-05-24 21:03:17 CDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-05-24 20:58:05 CDT";
-  public static final long BUILD_UNIX_TIME = 1748138285624L;
+  public static final String BUILD_DATE = "2025-05-25 00:33:44 CDT";
+  public static final long BUILD_UNIX_TIME = 1748151224845L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
